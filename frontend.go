0b792ce4fbbc7caea631eb1cab98208d4baffa16
Daniel Porteous <danielporteous1@gmail.com>
2020-05-17 08:29:46 -0700

    Add my own google analytics

--- frontend.go
+++ frontend.go
@@ -11,6 +11,15 @@ const tpl = `
 <!DOCTYPE html>
 <html>
     <head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166905893-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-166905893-1');
+		</script>
         <title>Codenames Pictures - Play Online</title>
         <script src="/js/lib/browser.min.js"></script>
         <script src="/js/lib/react.min.js"></script>
@@ -69,16 +78,6 @@ const tpl = `
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     </head>
     <body>
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-88084599-2', 'auto');
-		  ga('send', 'pageview');
-
-		</script>
 		<div id="app">
 		</div>
         <script type="text/babel">
